c5f5f0551413ebd09bdb8bd881a756f72451532f
dkormann <dominikormann@outlook.com>
2024-01-10 15:45:31 +0100

    fix

--- src/routes/util.ts
+++ src/routes/util.ts
@@ -61,7 +61,7 @@ export function is_link(txt:string):boolean{
 
 export function is_name(txt:string){
 
-    for (let part of txt.split(/\.,\:/)){
+    for (let part of txt.split(/\.|\:/)){
         console.log(part);
         if (!/^[a-z0-9,ß,ä,ö,ü,ø,_]+$/i.test(part)){
             return false
